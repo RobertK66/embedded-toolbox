6a1ae3545753504f79861a02b978190882028980
RobertK66 <RobertK66@users.noreply.github.com>
2021-08-26 13:14:33 +0200

    first commit

---
+++ ScreenLib/ScreenArea.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ScreenLib
+{
+    public class ScreenArea
+    {
+        public void SayHello() {
+            Console.WriteLine("Hello fröm Screen Area");
+        }
+    }
+}
---
+++ StatusConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ScreenLib;
+
+namespace StatusConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ScreenArea sca = new ScreenArea();
+            sca.SayHello();
+            Console.ReadLine();
+        }
+    }
+}
