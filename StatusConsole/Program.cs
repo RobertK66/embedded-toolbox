a15d171c1f5a8167e2955947025bf47785195453
RobertK66 <RobertK66@users.noreply.github.com>
2021-08-26 13:28:17 +0200

    add line to gitignore

--- StatusConsole/Program.cs
+++ StatusConsole/Program.cs
@@ -7,7 +7,7 @@ namespace StatusConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hellö Würld!");
 
             ScreenArea sca = new ScreenArea();
             sca.SayHello();
