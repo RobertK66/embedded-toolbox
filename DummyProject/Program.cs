de8424c4ac8d558ec7470de7fd98fd10a0ef2085
RobertK66 <RobertK66@users.noreply.github.com>
2024-02-05 08:16:08 +0100

    x

--- DummyProject/Program.cs
+++ DummyProject/Program.cs
@@ -1,5 +1,7 @@
 namespace DummyProject {
+    // This project only exists to make the github Actions node appear in the Solution Explorer !!!!
     public class Program {
+        public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
